f171069a3fef5d9c554fde6ae44f886d7245401c
uoon-dev <uoon.dev@gmail.com>
2018-09-11 20:26:53 +0900

    add comments

--- todo.js
+++ todo.js
@@ -6,17 +6,31 @@ const toDoList = {
 
 let index = 1;
 
+/**
+ * 
+ * @param {String} state : 추가하고 싶은 할 일 이름
+ */
 const add = (state) => {
   toDoList.todo.push({index, state});
   return console.log(`id: ${index++}, ${state} 항목이 새로 추가되었습니다.`);
 }
 
+/**
+ * 
+ * @param {String} state : 출력하고 싶은 상태 
+ */
 const show = (state) => {
   toDoList[state].forEach((value) => {
     return console.log(`${state}:${value.index}. ${value.state}`)
   })
 }
 
+
+/**
+ * 
+ * @param {String} id : 할 일의 ID 값
+ * @param {String} state : ID를 옮길 상태 장소
+ */
 const update = (id, state) => {
   let job;
   for (item in toDoList) {
@@ -33,6 +47,10 @@ const update = (id, state) => {
   return console.log(`현재상태 : todo:${toDoList['todo'].length}개, doing:${toDoList['doing'].length}개, done:${toDoList['done'].length}개`);
 }
 
+/**
+ * 
+ * @param {String} controlText : 커맨드 
+ */
 const executeCommand = (controlText) => {
   const commandValues = controlText.split('$');
 
