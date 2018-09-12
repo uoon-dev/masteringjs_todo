c0d94701d4715a8168d9a9c30bb6ca00ac5633e3
uoon lee <uoon.dev@jobis.co>
2018-09-12 14:05:16 +0900

    use the curry pattern for pipe line

--- todo.js
+++ todo.js
@@ -1,77 +1,91 @@
+let toDoIndex = 1;
+
 const toDoList = {
   todo: [],
   doing: [],
   done: []
 }
 
-let index = 1;
-
-/**
- * 
- * @param {String} state : 추가하고 싶은 할 일 이름
- */
-const add = (state) => {
-  toDoList.todo.push({index, state});
-  return console.log(`id: ${index++}, ${state} 항목이 새로 추가되었습니다.`);
-}
-
-/**
- * 
- * @param {String} state : 출력하고 싶은 상태 
- */
-const show = (state) => {
-  toDoList[state].forEach((value) => {
-    return console.log(`${state}:${value.index}. ${value.state}`)
-  })
-}
-
-
-/**
- * 
- * @param {String} id : 할 일의 ID 값
- * @param {String} state : ID를 옮길 상태 장소
- */
-const update = (id, state) => {
-  let job;
-  for (item in toDoList) {
-    toDoList[item].forEach((value, key) => {
-      if (value.index == id) {
-        job = Object.assign({}, toDoList[item][key]);
-        delete toDoList[item][key];
-      }
+const methods = {
+  /**
+   * 
+   * @param {String} state : 추가하고 싶은 할 일 이름
+   */
+  add(state) {
+    toDoList.todo.push({toDoIndex, state});
+    console.log(`id: ${toDoIndex++}, ${state} 항목이 새로 추가되었습니다.`);
+    return 'add'
+  },
+  /**
+   * 
+   * @param {String} state : 출력하고 싶은 상태 
+   */
+  show(state) {
+    toDoList[state].forEach((value) => {
+      console.log(`${state}: ${value.toDoIndex}. ${value.state}`)
+      return 'show'
     })
-    toDoList[item] = toDoList[item].filter(item => item !== undefined);
-  }
+  },
+  /**
+   * 
+   * @param {String} id : 할 일의 ID 값
+   * @param {String} state : ID를 옮길 상태 장소
+   */
+  update(id, state) {
+    let job;
 
-  toDoList[state].push(job);
-  return console.log(`현재상태 : todo:${toDoList['todo'].length}개, doing:${toDoList['doing'].length}개, done:${toDoList['done'].length}개`);
-}
+    Object.keys(toDoList).forEach(unit => {
+        toDoList[unit].some((item, index) => {
+          if (item.toDoIndex === parseInt(id)) {
+            job = item;
+            delete toDoList[unit][index];
+          }
+        })
+        // delete 해서 undefined가 된 값을 제외하고 반환 
+        toDoList[unit] = toDoList[unit].filter(x => x);
+    })
+
+    toDoList[state].push(job);
+  
+    console.log(`현재상태 : todo:${toDoList['todo'].length}개, doing:${toDoList['doing'].length}개, done:${toDoList['done'].length}개`);
+    return 'update';
+  }
+};
 
 /**
  * 
- * @param {String} controlText : 커맨드 
+ * @param {String} parseCommand : 커맨드 
  */
-const executeCommand = (controlText) => {
-  const commandValues = controlText.split('$');
+const executeCommand = (parseCommand) => {
+  const method = parseCommand[0];
+  const state = parseCommand.length === 2 ? parseCommand[1] : parseCommand[2];
+  const id = parseCommand.length === 3 ? parseCommand[1] : '';
 
-  switch (commandValues.length) {
-    case 2: {
-      const [fn, state] = commandValues.map((value) => value.toString().toLowerCase().trim());
-      eval(`${fn}('${state}')`); break;
-    }
-    case 3: {
-      const [fn, id, state] = commandValues.map((value) => value.toString().toLowerCase().trim());
-      eval(`${fn}('${id}', '${state}')`); break;
-    }
+  switch(method) {
+    case 'add': methods[method](state); break;
+    case 'show': methods[method](state); break;
+    case 'update': methods[method](id, state); break;
   }
 }
 
-const command = (controlText) => executeCommand(controlText); 
+const getParsedCommand = (commandText) => commandText.split('$').map((value) => value.toString().toLowerCase().trim());
 
-command('aDD$ 투두리스트 만들기');
-command('updAte$1$doing');
-command('add$자바스크립트 만들기');
+/**
+ * 
+ * @param {*} methods : 실행시키고 싶은 함수들
+ */
+const commandPipe = (...methods) => (args) => methods.reduce((arg, nextFn) => nextFn(arg), args);
+const command = (commandText) => {
+  commandPipe(
+    getParsedCommand,
+    executeCommand
+  )(commandText);
+}
+
 command('add$ 게임 만들기');
-command('update$2$doing');
-command('  update$3$done');
-command('   show$ DOING');
+command('add$ 투두리스트 만들기');
+command('show$ todo');
+command('Update$1$done');
+command('show$ done');
+command('Update$2$done');
+command('show$2$done');
